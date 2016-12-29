f1506fbf48f8a77b4b2cf3cdef3c65d55a2183e2
Yuki Reii <Yuki Reii>
2016-12-29 22:07:06 +0700

    Key Down Script

---
+++ Assets/Control.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    private Rigidbody2D physic2D;
+
+    void Awake()
+    {
+        physic2D = GetComponent<Rigidbody2D>();
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("down"))
+            print("down arrow key is held down");
+    }
+
+
+}
