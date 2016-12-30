059ab6af46a886e2c96455f9fd8b8af5039422d4
Pakpon Jetapai <pakpon.j@ku.th>
2016-12-30 20:04:59 +0700

    Expose force vectors (up, left, and right). Add Top and Bottom 2d colliders.

--- Assets/Control.cs
+++ Assets/Control.cs
@@ -6,6 +6,10 @@ public class Control : MonoBehaviour {
 
     private Rigidbody2D physic2D;
 
+	public Vector2 upForce = new Vector2 (0f, 15f);
+	public Vector2 leftForce = new Vector2 (5f, 0f);
+	public Vector2 rightForce = new Vector2 (-5f, 0f);
+
     void Awake()
     {
         physic2D = GetComponent<Rigidbody2D>();
@@ -14,11 +18,11 @@ public class Control : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKeyDown(KeyCode.DownArrow))
-            physic2D.AddForce(new Vector2(0f, 30f));
+			physic2D.AddForce( upForce );
         if (Input.GetKeyDown(KeyCode.RightArrow))
-            physic2D.AddForce(new Vector2(5f, 0f));
+            physic2D.AddForce( leftForce );
         if (Input.GetKeyDown(KeyCode.LeftArrow))
-            physic2D.AddForce(new Vector2(-5f, 0f));
+            physic2D.AddForce( rightForce );
     }
 
 
